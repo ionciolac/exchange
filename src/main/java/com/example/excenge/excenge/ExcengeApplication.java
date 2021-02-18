77fa53746507daa4b5e52b21d37ae1495720bb37
ionciolac <wowgreep@gmail.com>
2021-02-18 17:46:44 +0200

    project init

---
+++ src/main/java/com/example/excenge/excenge/ExcengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.excenge.excenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/excenge/excenge/ExcengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.excenge.excenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
